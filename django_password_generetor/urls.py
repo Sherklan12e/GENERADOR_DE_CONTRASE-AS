4c9f64ac94f1bd3a6aa2364cda8616b03ae119da
Sherklan12e <davismendoza.08@gmail.com>
2023-01-29 05:48:39 -0300

    mensaje del commit

---
+++ django_password_generetor/urls.py
@@ -0,0 +1,9 @@
+# from django.contrib import admin
+from django.urls import path
+from generator import views
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path('about/', views.about, name='about'),
+    path('', views.home, name='home'),
+    path('generate-password', views.password, name='password'),
+]
